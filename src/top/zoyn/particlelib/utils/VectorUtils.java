96d32b20cce5086ded4358ec4a50d4981f7bba90
Zoyn <602723113@qq.com>
2020-09-20 22:54:12 +0800

    update: 从EffectLib复制了一份利用yaw和pitch旋转向量的方法

--- src/top/zoyn/particlelib/utils/VectorUtils.java
+++ src/top/zoyn/particlelib/utils/VectorUtils.java
@@ -31,4 +31,43 @@ public class VectorUtils {
         double y = v.getX() * sin + v.getY() * cos;
         return v.setX(x).setY(y);
     }
+
+    /**
+     * This handles non-unit vectors, with yaw and pitch instead of X,Y,Z angles.
+     * <p>
+     * Thanks to SexyToad!
+     * <p>
+     * 将一个非单位向量使用yaw和pitch来代替X, Y, Z的角旋转方式
+     *
+     * @param v            向量
+     * @param yawDegrees   yaw的角度
+     * @param pitchDegrees pitch的角度
+     * @return {@link Vector}
+     */
+    public static Vector rotateVector(Vector v, float yawDegrees, float pitchDegrees) {
+        double yaw = Math.toRadians(-1 * (yawDegrees + 90));
+        double pitch = Math.toRadians(-pitchDegrees);
+
+        double cosYaw = Math.cos(yaw);
+        double cosPitch = Math.cos(pitch);
+        double sinYaw = Math.sin(yaw);
+        double sinPitch = Math.sin(pitch);
+
+        double initialX, initialY, initialZ;
+        double x, y, z;
+
+        // Z_Axis rotation (Pitch)
+        initialX = v.getX();
+        initialY = v.getY();
+        x = initialX * cosPitch - initialY * sinPitch;
+        y = initialX * sinPitch + initialY * cosPitch;
+
+        // Y_Axis rotation (Yaw)
+        initialZ = v.getZ();
+        initialX = x;
+        z = initialZ * cosYaw - initialX * sinYaw;
+        x = initialZ * sinYaw + initialX * cosYaw;
+
+        return new Vector(x, y, z);
+    }
 }
