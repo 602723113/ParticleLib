4bbae3b94d05a3c9ac6a410dc40bb0334528ebf2
Zoyn <602723113@qq.com>
2023-01-19 18:36:14 +0800

    更新: 给 Star 增加 Playable 的方法

--- src/main/java/top/zoyn/particlelib/pobject/Star.java
+++ src/main/java/top/zoyn/particlelib/pobject/Star.java
@@ -1,7 +1,9 @@
 package top.zoyn.particlelib.pobject;
 
 import org.bukkit.Location;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
+import top.zoyn.particlelib.ParticleLib;
 import top.zoyn.particlelib.utils.VectorUtils;
 
 /**
@@ -9,10 +11,17 @@ import top.zoyn.particlelib.utils.VectorUtils;
  *
  * @author Zoyn
  */
-public class Star extends ParticleObject {
+public class Star extends ParticleObject implements Playable {
 
     private final double radius;
     private final double step;
+
+    private final double length;
+    private int currentSide = 1;
+    private double currentStep = 0;
+
+    private Vector changeableStart;
+    private Location changableEnd;
 
     public Star(Location origin) {
         this(origin, 1, 0.05);
@@ -22,11 +31,22 @@ public class Star extends ParticleObject {
         setOrigin(origin);
         this.radius = radius;
         this.step = step;
+
+        // 每条线的长度
+        this.length = Math.cos(Math.toRadians(36)) * radius * 2;
+
+        changeableStart = new Vector(1, 0, 0);
+        // 转弧度制
+        final double radians = Math.toRadians(72 * 2);
+        final double x = radius * Math.cos(radians);
+        final double y = 0D;
+        final double z = radius * Math.sin(radians);
+        changableEnd = getOrigin().clone().add(x, y, z);
     }
 
     @Override
     public void show() {
-        Vector v = new Vector(1, 0, 0);
+        Vector START = new Vector(1, 0, 0);
         // 转弧度制
         double radians = Math.toRadians(72 * 2);
         double x = radius * Math.cos(radians);
@@ -34,18 +54,86 @@ public class Star extends ParticleObject {
         double z = radius * Math.sin(radians);
         Location end = getOrigin().clone().add(x, y, z);
 
-        double length = Math.cos(Math.toRadians(36)) * radius * 2;
         for (int i = 1; i <= 5; i++) {
             for (double j = 0; j < length; j += step) {
-                Vector vectorTemp = v.clone().multiply(j);
+                Vector vectorTemp = START.clone().multiply(j);
                 Location spawnLocation = end.clone().add(vectorTemp);
 
                 spawnParticle(spawnLocation);
             }
-            Vector vectorTemp = v.clone().multiply(length);
+            Vector vectorTemp = START.clone().multiply(length);
             end = end.clone().add(vectorTemp);
 
-            VectorUtils.rotateAroundAxisY(v, -144);
+            VectorUtils.rotateAroundAxisY(START, -144);
+        }
+    }
+
+    @Override
+    public void play() {
+        new BukkitRunnable() {
+            final Vector START = new Vector(1, 0, 0);
+            // 转弧度制
+            final double radians = Math.toRadians(72 * 2);
+            final double x = radius * Math.cos(radians);
+            final double y = 0D;
+            final double z = radius * Math.sin(radians);
+            Location end = getOrigin().clone().add(x, y, z);
+
+            @Override
+            public void run() {
+                // 进行关闭
+                if (currentSide >= 6) {
+                    cancel();
+                    return;
+                }
+                if (currentStep > length) {
+                    // 切换到下一条边开始
+                    currentSide += 1;
+                    currentStep = 0;
+
+                    Vector vectorTemp = START.clone().multiply(length);
+                    end = end.clone().add(vectorTemp);
+
+                    VectorUtils.rotateAroundAxisY(START, -144);
+                }
+                Vector vectorTemp = START.clone().multiply(currentStep);
+                Location spawnLocation = end.clone().add(vectorTemp);
+
+                spawnParticle(spawnLocation);
+                currentStep += step;
+            }
+        }.runTaskTimer(ParticleLib.getInstance(), 0, getPeriod());
+    }
+
+    @Override
+    public void playNextPoint() {
+        Vector vectorTemp = changeableStart.clone().multiply(currentStep);
+        Location spawnLocation = changableEnd.clone().add(vectorTemp);
+
+        spawnParticle(spawnLocation);
+        currentStep += step;
+        if (currentStep > length) {
+            // 切换到下一条边开始
+            currentSide += 1;
+            currentStep = 0;
+
+            vectorTemp = changeableStart.clone().multiply(length);
+            changableEnd = changableEnd.clone().add(vectorTemp);
+
+            VectorUtils.rotateAroundAxisY(changeableStart, -144);
+        }
+
+        // 重置
+        if (currentSide >= 6) {
+            currentSide = 1;
+            currentStep = 0;
+            changeableStart = new Vector(1, 0, 0);
+            // 转弧度制
+            final double radians = Math.toRadians(72 * 2);
+            final double x = radius * Math.cos(radians);
+            final double y = 0D;
+            final double z = radius * Math.sin(radians);
+            changableEnd = getOrigin().clone().add(x, y, z);
         }
     }
 }
