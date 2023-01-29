38426bfd12c7431c9147a7817016c9c0ecf4cae3
Zoyn <602723113@qq.com>
2023-01-29 13:59:38 +0800

    更新: 特效组支持设置更多的参数, 且统一设置为操作 3行3列 的矩阵

--- src/main/java/top/zoyn/particlelib/pobject/EffectGroup.java
+++ src/main/java/top/zoyn/particlelib/pobject/EffectGroup.java
@@ -1,6 +1,8 @@
 package top.zoyn.particlelib.pobject;
 
 import com.google.common.collect.Lists;
+import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Entity;
 import top.zoyn.particlelib.utils.matrix.Matrixs;
@@ -12,7 +14,8 @@ import java.util.List;
  * 代表一个特效组
  * <p>
  * 如果你要使用 EffectGroup#scale 这样的方法, 我不建议你将 2D 的特效和 3D 的特效放在一起
- * </p>
+ * <p>
+ * EffectGroup 的矩阵默认使用 3行3列 的矩阵进行变换
  *
  * @author Zoyn IceCold
  */
@@ -25,6 +28,10 @@ public class EffectGroup {
 
     public EffectGroup() {
         effectList = Lists.newArrayList();
+    }
+
+    public EffectGroup(ParticleObject... effects) {
+        effectList = Lists.newArrayList(effects);
     }
 
     /**
@@ -70,6 +77,110 @@ public class EffectGroup {
     }
 
     /**
+     * 将特效组内的特效进行缩小或扩大
+     *
+     * @param value 缩小或扩大的倍率
+     * @return {@link EffectGroup}
+     */
+    public EffectGroup scale(double value) {
+        effectList.forEach(effect -> effect.addMatrix(Matrixs.scale(3, 3, value)));
+        return this;
+    }
+
+    /**
+     * 将特效组内的特效绕 X 轴进行旋转
+     *
+     * @param angle 旋转角度
+     * @return {@link EffectGroup}
+     */
+    public EffectGroup rotateAroundXAxis(double angle) {
+        effectList.forEach(effect -> effect.addMatrix(Matrixs.rotateAroundXAxis(angle)));
+        return this;
+    }
+
+    /**
+     * 将特效组内的特效绕 Y 轴进行旋转
+     *
+     * @param angle 旋转角度
+     * @return {@link EffectGroup}
+     */
+    public EffectGroup rotateAroundYAxis(double angle) {
+        effectList.forEach(effect -> effect.addMatrix(Matrixs.rotateAroundYAxis(angle)));
+        return this;
+    }
+
+    /**
+     * 将特效组内的特效绕 Z 轴进行旋转
+     *
+     * @param angle 旋转角度
+     * @return {@link EffectGroup}
+     */
+    public EffectGroup rotateAroundZAxis(double angle) {
+        effectList.forEach(effect -> effect.addMatrix(Matrixs.rotateAroundZAxis(angle)));
+        return this;
+    }
+
+    public EffectGroup attachEntity(Entity entity) {
+        effectList.forEach(effect -> effect.attachEntity(entity));
+        return this;
+    }
+
+    public EffectGroup setOrigin(Location origin) {
+        effectList.forEach(effect -> effect.setOrigin(origin));
+        return this;
+    }
+
+    public EffectGroup setParticle(Particle particle) {
+        effectList.forEach(effect -> effect.setParticle(particle));
+        return this;
+    }
+
+    public EffectGroup setCount(int count) {
+        effectList.forEach(effect -> effect.setCount(count));
+        return this;
+    }
+
+    public EffectGroup setOffsetX(double offsetX) {
+        effectList.forEach(effect -> effect.setOffsetX(offsetX));
+        return this;
+    }
+
+    public EffectGroup setOffsetY(double offsetY) {
+        effectList.forEach(effect -> effect.setOffsetX(offsetY));
+        return this;
+    }
+
+    public EffectGroup setOffsetZ(double offsetZ) {
+        effectList.forEach(effect -> effect.setOffsetX(offsetZ));
+        return this;
+    }
+
+    public EffectGroup setExtra(double extra) {
+        effectList.forEach(effect -> effect.setExtra(extra));
+        return this;
+    }
+
+    public EffectGroup setData(Object data) {
+        effectList.forEach(effect -> effect.setData(data));
+        return this;
+    }
+
+    public EffectGroup setIncrementX(double incrementX) {
+        effectList.forEach(effect -> effect.setIncrementX(incrementX));
+        return this;
+    }
+
+    public EffectGroup setIncrementY(double incrementY) {
+        effectList.forEach(effect -> effect.setIncrementX(incrementY));
+        return this;
+    }
+
+    public EffectGroup setIncrementZ(double incrementZ) {
+        effectList.forEach(effect -> effect.setIncrementX(incrementZ));
+        return this;
+    }
+
+    /**
      * 利用给定的数字, 设置每一个特效的循环 tick
      *
      * @param period 循环tick
@@ -80,25 +191,8 @@ public class EffectGroup {
         return this;
     }
 
-    /**
-     * 将特效组内的特效进行缩小或扩大
-     *
-     * @param value 缩小或扩大的倍率
-     * @return {@link EffectGroup}
-     */
-    public EffectGroup scale(double value) {
-        effectList.forEach(effect -> effect.addMatrix(Matrixs.scale(2, 2, value)));
-        return this;
-    }
-
-    /**
-     * 将特效组内的特效进行旋转
-     *
-     * @param angle 旋转角度
-     * @return {@link EffectGroup}
-     */
-    public EffectGroup rotate(double angle) {
-        effectList.forEach(effect -> effect.addMatrix(Matrixs.rotate2D(angle)));
+    public EffectGroup setColor(Color color) {
+        effectList.forEach(effect -> effect.setColor(color));
         return this;
     }
 
@@ -172,16 +266,6 @@ public class EffectGroup {
                 pObj.alwaysPlayAsync();
             }
         }
-        return this;
-    }
-
-    public EffectGroup attachEntity(Entity entity) {
-        effectList.forEach(effect -> effect.attachEntity(entity));
-        return this;
-    }
-
-    public EffectGroup setParticle(Particle particle) {
-        effectList.forEach(effect -> effect.setParticle(particle));
         return this;
     }
 
